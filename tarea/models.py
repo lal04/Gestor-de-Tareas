883146e5c81984ce13d8ed7b84576e142ae27a1a
raul <raulzambranoleon15@gmail.com>
2025-01-07 17:00:25 -0500

    agregar estilos

--- tarea/models.py
+++ tarea/models.py
@@ -1,10 +1,14 @@
 from django.db import models
 
 class Tarea(models.Model):
+    ESTADO_CHOICES=(
+        ('pendiente', 'Pendiente'),
+        ('completada', 'Completada')
+    )
     fecha=models.DateTimeField(auto_now_add=True)
     nombre=models.CharField(max_length=100)
     descripcion=models.TextField(null=True, blank=True, default="sin Descripcion")
-    estado=models.BooleanField(default=False,)
+    estado=models.CharField(max_length=50, choices=ESTADO_CHOICES, default='pendiente')
     
     def __str__(self):
         return self.nombre
